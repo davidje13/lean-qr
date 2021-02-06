b56a4eb4a3e89f61f17be28e1ff4757a7daf0c9b
David Evans <davidje13@users.noreply.github.com>
2021-02-06 20:02:09 +0000

    Add colour choice to web UI

--- web/index.mjs
+++ web/index.mjs
@@ -1,11 +1,23 @@
 import { correction, generate } from '../src/index.mjs';
 
+function getInput(name) {
+  return document.querySelector(`[name="${name}"]`);
+}
+
 function getValue(name) {
-  return document.querySelector(`[name="${name}"]`).value;
+  return getInput(name).value;
 }
 
 function getInt(name) {
-  return Math.round(Number(getValue(name)));
+  return Math.round(Number.parseInt(getValue(name), 10));
+}
+
+function getColour(name) {
+  const rgb = Number.parseInt(getValue(name).substr(1), 16);
+  const r = (rgb >> 16) & 0xFF;
+  const g = (rgb >> 8) & 0xFF;
+  const b = rgb & 0xFF;
+  return 0xFF000000 | (b << 16) | (g << 8) | r;
 }
 
 function getMask() {
@@ -27,7 +39,8 @@ function regenerate() {
       maxCorrectionLevel: correction[getValue('max-correction')],
       mask: getMask(),
     });
-    code.toCanvas(output);
+    code.toCanvas(output, { on: getColour('on'), off: getColour('off') });
+    output.style.background = getValue('off');
     err.style.display = 'none';
     output.style.display = 'block';
   } catch (e) {
@@ -38,5 +51,14 @@ function regenerate() {
 }
 
 document.querySelectorAll('input, select, textarea').forEach((o) => o.addEventListener('input', regenerate));
+document.getElementById('reverse-col').addEventListener('click', () => {
+  const on = getInput('on');
+  const off = getInput('off');
+  const onV = on.value;
+  const offV = off.value;
+  on.value = offV;
+  off.value = onV;
+  regenerate();
+});
 
 regenerate();
