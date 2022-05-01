6eb63bb78dc48d90d35170ec744f5a2da73b882e
David Evans <davidje13@users.noreply.github.com>
2022-05-01 13:47:06 +0100

    Record library size when testing for comparing new versions

--- test-package/run.sh
+++ test-package/run.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 set -e
 
+BASE_DIR="$(cd "$(dirname "$0")/.."; pwd)";
+cd "$BASE_DIR";
+
+echo "Building library...";
+echo;
+npm run build;
+RAW_SIZE="$(wc -c < build/index.js)";
+COMPRESSED_SIZE="$(zip -jqX9 - build/index.js | wc -c)";
+
+printf "Library size:    $RAW_SIZE bytes\nCompressed size: $COMPRESSED_SIZE bytes\n" > docs/stats.txt;
+
 echo "Running package test...";
 echo;
 
-BASE_DIR="$(cd "$(dirname "$0")/.."; pwd)";
-cd "$BASE_DIR";
 rm /lean-qr-*.tgz 2>/dev/null || true;
 npm pack;
 rm test-package/lean-qr.tgz 2>/dev/null || true;
@@ -13,7 +22,7 @@ mv lean-qr-*.tgz test-package/lean-qr.tgz;
 cd - >/dev/null;
 
 cd "$BASE_DIR/test-package";
-rm -rf node_modules || true;
+rm -rf node_modules/lean-qr || true;
 npm install --audit=false;
 rm lean-qr.tgz || true;
 npm test --ignore-scripts=false; # ignore-scripts is over-zealous on Node 14
@@ -21,4 +30,5 @@ cd - >/dev/null;
 
 echo;
 echo "Package test complete";
+cat "$BASE_DIR/docs/stats.txt";
 echo;
