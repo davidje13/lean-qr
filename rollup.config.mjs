c4d8cfa7d5db5959fc9c3b12b632390981c88612
David Evans <davidje13@users.noreply.github.com>
2023-02-11 14:15:01 +0000

    Remove babel preset-env from build, as it is only applying unnecessary conversions for the target browsers

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,27 +2,7 @@ import terser from '@rollup/plugin-terser';
 import { babel } from '@rollup/plugin-babel';
 
 const plugins = [
-  babel({
-    babelHelpers: 'bundled',
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          useBuiltIns: false,
-          targets: {
-            node: '14',
-            browsers: [
-              'last 1 chrome version',
-              'last 1 firefox version',
-              'last 1 ios version',
-              //'last 1 edge version',
-              //'last 1 safari version',
-            ],
-          },
-        },
-      ],
-    ],
-  }),
+  babel({ babelHelpers: 'bundled' }),
   terser({ format: { ascii_only: true } }),
 ];
 
