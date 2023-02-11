309079c7645cec7c025e8921584814cbe5bb2107
David Evans <davidje13@users.noreply.github.com>
2023-02-11 14:58:08 +0000

    Simplify rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,13 @@
 import terser from '@rollup/plugin-terser';
 import { babel } from '@rollup/plugin-babel';
 
-const plugins = [
-  babel({ babelHelpers: 'bundled' }),
-  terser({ format: { ascii_only: true } }),
-];
+const target = (path) => ({
+  input: `src/${path}.mjs`,
+  output: [{ file: `build/${path}.js`, format: 'cjs' }],
+  plugins: [
+    babel({ babelHelpers: 'bundled' }),
+    terser({ format: { ascii_only: true } }),
+  ],
+});
 
-export default [
-  {
-    input: 'src/index.mjs',
-    output: {
-      file: 'build/index.js',
-      format: 'cjs',
-    },
-    plugins,
-  },
-  {
-    input: 'src/extras/svg.mjs',
-    output: {
-      file: 'build/extras/svg.js',
-      format: 'cjs',
-    },
-    plugins,
-  },
-];
+export default [target('index'), target('extras/svg')];
