9107964170150af8db039c827c92c2809ee750a0
David Evans <davidje13@users.noreply.github.com>
2023-02-14 22:07:08 +0000

    Create bundle for web scripts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,24 @@
 import terser from '@rollup/plugin-terser';
 import { babel } from '@rollup/plugin-babel';
 
+const plugins = [
+  babel({ babelHelpers: 'bundled' }),
+  terser({ format: { ascii_only: true } }),
+];
+
 const target = (path) => ({
   input: `src/${path}.mjs`,
   output: [{ file: `build/${path}.js`, format: 'cjs' }],
-  plugins: [
-    babel({ babelHelpers: 'bundled' }),
-    terser({ format: { ascii_only: true } }),
-  ],
+  plugins,
 });
 
-export default [target('index'), target('extras/svg'), target('extras/jis')];
+export default [
+  target('index'),
+  target('extras/svg'),
+  target('extras/jis'),
+  {
+    input: 'web/index.mjs',
+    output: [{ file: 'web/build/index.min.mjs', format: 'esm' }],
+    plugins,
+  },
+];
