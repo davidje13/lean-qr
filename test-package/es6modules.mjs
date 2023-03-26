39fbf6c026e2035526ab7d96f99be1db6cfbdc2d
David Evans <davidje13@users.noreply.github.com>
2023-03-26 12:35:15 +0100

    Add package test for functionality (test minification)

--- test-package/es6modules.mjs
+++ test-package/es6modules.mjs
@@ -15,3 +15,39 @@ if (typeof toSvgSource !== 'function') {
 if (typeof shift_jis !== 'function') {
   throw new Error("import 'lean-qr/extras/jis' did not return shift_jis mode");
 }
+
+const expected = `-----------------------------
+-----------------------------
+-----------------------------
+-----------------------------
+----ooooooo---ooo-ooooooo----
+----o-----o-o-o-o-o-----o----
+----o-ooo-o--o--o-o-ooo-o----
+----o-ooo-o-o-o-o-o-ooo-o----
+----o-ooo-o-ooo---o-ooo-o----
+----o-----o---ooo-o-----o----
+----ooooooo-o-o-o-ooooooo----
+------------ooooo------------
+-----o-oooo-oo---oo-oo-o-----
+----o---oo-o-o-oo-o-o-oo-----
+------o-ooo--oo--oo----o-----
+----o----o-oo--ooo-o-o-oo----
+----o-oo--o-o-oo-oooo--o-----
+------------o---o-o---o------
+----ooooooo----o--o--o-oo----
+----o-----o-o-oo-ooo-o-o-----
+----o-ooo-o-oo-o-o-oo-ooo----
+----o-ooo-o-oooooo-oooo------
+----o-ooo-o--ooooo-oo--oo----
+----o-----o-ooo-oooooo-o-----
+----ooooooo--o--ooo-o--o-----
+-----------------------------
+-----------------------------
+-----------------------------
+-----------------------------
+`;
+
+const sample = generate('LEAN-QR LIBRARY').toString({ on: 'o', off: '-' });
+if (sample !== expected) {
+  throw new Error("import 'lean-qr' produced incorrect output\n" + sample);
+}
