9813626207070cc1b100782381b588944e825eb0
David Evans <davidje13@users.noreply.github.com>
2025-05-27 15:15:58 +0100

    breaking: remove deprecated generate.with and optimise internal handling of mode exports

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,27 @@
 export { correction } from './qr/options/corrections.mjs';
-export { mode } from './qr/options/modes.mjs';
+import {
+  multi,
+  eci,
+  bytes,
+  numeric,
+  alphaNumeric,
+  ascii,
+  iso8859_1,
+  utf8,
+  shift_jis,
+  auto,
+} from './qr/options/modes.mjs';
 export { generate } from './qr/generate.mjs';
+
+export const mode = {
+  multi,
+  eci,
+  bytes,
+  numeric,
+  alphaNumeric,
+  ascii,
+  iso8859_1,
+  utf8,
+  shift_jis,
+  auto,
+};
